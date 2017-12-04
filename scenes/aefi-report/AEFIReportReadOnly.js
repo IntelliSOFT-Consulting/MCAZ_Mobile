5247ff7fb4f9a2e64739104b015207dfa90b279b
John Kenny Muiruri <john.k.muiruri@gmail.com>
2017-12-04 15:38:35 +0300

    fixed read only aefi-report

--- scenes/aefi-report/AEFIReportReadOnly.js
+++ scenes/aefi-report/AEFIReportReadOnly.js
@@ -23,7 +23,7 @@ export default class AEFIReportReadOnly extends Component{
         <ReadOnlyDataRenderer label="Patient’s physical address:" name="patient_address" model={ model } required={ true }/>
         <ReadOnlyDataRenderer label="Telephone:" keyboardType="phone-pad" name="patient_telephone" model={ model }/>
         <ReadOnlyDataRenderer label="Gender:" name="gender" model={ model } options={ GENDER }/>
-        <ReadOnlyDataRenderer label="Date of birth (DD/MM/YYYY):" name="date_of_birth" model={ model } required={ true }/>
+        <ReadOnlyDataRenderer label="Date of birth (DD/MM/YYYY):" name="date_of_birth" model={ model } required={ true } type="date"/>
         <ReadOnlyDataRenderer label="OR Age at onset" name="age_at_onset" model={ model }/>
         <ReadOnlyDataRenderer label="Specify" name="age_at_onset_specify" model={ model }/>
         <ReadOnlyDataRenderer label="Reporter’s Name:" name="reporter_name" model={ model } required={ true }/>
@@ -43,24 +43,24 @@ export default class AEFIReportReadOnly extends Component{
         <ReadOnlyDataRenderer label="Adverse event (s):" name="adverse_events" model={ model } required={ true } options={ [] }/>
 
         <ReadOnlyDataRenderer label="Other" name="adverse_events_specify" model={ model }/>
-        <ReadOnlyDataRenderer label="Date &amp; Time AEFI started (DD/MM/YYYY):" name="aefi_date" model={ model } showTime={ true }/>
-        <ReadOnlyDataRenderer label="Date patient notified event to health system (DD/MM/YYYY):" name="notification_date" model={ model }/>
+        <ReadOnlyDataRenderer label="Date &amp; Time AEFI started (DD/MM/YYYY)" name="aefi_date" model={ model } showTime={ true } type="date"/>
+        <ReadOnlyDataRenderer label="Date patient notified event to health system (DD/MM/YYYY)" name="notification_date" model={ model } type="date"/>
         <ReadOnlyDataRenderer label="Describe AEFI (Signs and symptoms):" multiline={true} numberOfLines={4} name="description_of_reaction" model={ model }/>
         <ReadOnlyDataRenderer label="Treatment provided:" options={ BOOLEAN_OPTIONS } name="treatment_provided" model={ model }/>
         <ReadOnlyDataRenderer label="Serious:" options={ BOOLEAN_OPTIONS } name="serious" model={ model } required={ true }/>
         <ReadOnlyDataRenderer label="If yes:" options={ AEFI_SEVERITY_REASON } name="serious_yes" model={ model }/>
         <ReadOnlyDataRenderer label="Outcome:" name="outcome" model={ model } required={ true } options={ OUTCOME }/>
-        <ReadOnlyDataRenderer label="If died, date of death (DD/MM/YYYY)::" name="died_date" model={ model }/>
+        <ReadOnlyDataRenderer label="If died, date of death (DD/MM/YYYY)::" name="died_date" model={ model } type="date"/>
         <ReadOnlyDataRenderer label="Autopsy done:" name="autopsy" model={ model } options={ BOOLEAN_UNKNOWN_OPTIONS }/>
         <ReadOnlyDataRenderer label="Past medical history (including history of similar reaction or other allergies), concomitant medication and other relevant information
             (e.g. other cases). Use additional sheet if needed :" multiline={true} numberOfLines={4} name="past_medical_history" model={ model }/>
 
         <Text>First decision making level to complete (District level):</Text>
-        <ReadOnlyDataRenderer label="Date report received at district level (DD/MM/YYYY):" name="district_receive_date" model={ model }/>
+        <ReadOnlyDataRenderer label="Date report received at district level (DD/MM/YYYY):" name="district_receive_date" model={ model } type="date"/>
         <ReadOnlyDataRenderer label="Investigation needed:" options={ BOOLEAN_OPTIONS } name="investigation_needed" model={ model }/>
-        <ReadOnlyDataRenderer label="If yes, date investigation planned (DD/MM/YYYY):" name="investigation_date" model={ model }/>
+        <ReadOnlyDataRenderer label="If yes, date investigation planned (DD/MM/YYYY):" name="investigation_date" model={ model } type="date"/>
         <Text>National level to complete:</Text>
-        <ReadOnlyDataRenderer label="Date report received at national level (DD/MM/YYYY):" name="national_receive_date" model={ model }/>
+        <ReadOnlyDataRenderer label="Date report received at national level (DD/MM/YYYY):" name="national_receive_date" model={ model } type="date"/>
         <ReadOnlyDataRenderer label="Comments:" name="comments" model={ model }/>
 
         <View style={ AppStyles.rowButtons }>
@@ -69,7 +69,4 @@ export default class AEFIReportReadOnly extends Component{
       </ScrollView>
     )
   }
-  // <Button title="Edit"/> <ConcomitantTableComponent model={ model } name="sadr_other_drugs" label="Concomitant (Other) drugs taken, including herbal medicines & Dates/period taken:"/>
-  // <MedicationTableComponent model={ model } name="sadr_list_of_drugs"/>
-  //<FileAttachmentComponent model={ model } name="files" label="Attach any files"/>
 }
