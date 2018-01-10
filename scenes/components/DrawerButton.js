9eee654ad629796c30f457e0472230e56ee967aa
John Kenny Muiruri <john.k.muiruri@gmail.com>
2018-01-10 09:18:53 +0300

    removed unused icons

--- scenes/components/DrawerButton.js
+++ scenes/components/DrawerButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { TouchableOpacity, Text, Image } from 'react-native'
 
 const DrawerButton = ({ navigation }) => (
-  <TouchableOpacity onPress={ () => navigation.navigate('DrawerOpen')} >
+  <TouchableOpacity onPress={ () => navigation.navigate('DrawerOpen') } >
     <Image source={ require("../../images/ic_menu_black_36dp_1x.png") }/>
   </TouchableOpacity>
 )
