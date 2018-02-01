4eeb346cd936f61cb6a4c8ff7b7bfb5c4d389ca9
John Kenny Muiruri <john.k.muiruri@gmail.com>
2018-02-01 07:15:17 +0300

    fixed xml structure

--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -97,40 +97,37 @@ class MainScene extends Component {
     reports.saefi = completed.filter(report => report.type == REPORT_TYPE_AEFI_INV)
 
     var x2js = new X2JS()
-
-    var sadr = {}
-    sadr.response = completed.filter(report => report.type == REPORT_TYPE_ADR).map((report) => {
+    var xmls = []
+    var output = {}
+    output.response = completed.filter(report => report.type == REPORT_TYPE_ADR).map((report) => {
       var r = {}
       r.sadrs = report
       return r
     })
-    xmls.push(x2js.json2xml_str(sadr))
+    //xmls.push(x2js.json2xml_str(sadr))
 
-    var adr = {}
-    adr.response = completed.filter(report => report.type == REPORT_TYPE_SAE).map((report) => {
+    output.response = output.response.concat(completed.filter(report => report.type == REPORT_TYPE_SAE).map((report) => {
       var r = {}
       r.adrs = report
       return r
-    })
-    xmls.push(x2js.json2xml_str(adr))
+    }))
+    //xmls.push(x2js.json2xml_str(adr))
 
-    var aefi = {}
-    aefi.response = completed.filter(report => report.type == REPORT_TYPE_AEFI).map((report) => {
+    output.response = output.response.concat(completed.filter(report => report.type == REPORT_TYPE_AEFI).map((report) => {
       var r = {}
       r.aefis = report
       return r
-    })
-    xmls.push(x2js.json2xml_str(aefi))
+    }))
+    //xmls.push(x2js.json2xml_str(aefi))
 
-    var saefi = {}
-    saefi.response = completed.filter(report => report.type == REPORT_TYPE_AEFI_INV).map((report) => {
+    output.response = output.response.concat(completed.filter(report => report.type == REPORT_TYPE_AEFI_INV).map((report) => {
       var r = {}
       r.saefis = report
       return r
-    })
-    xmls.push(x2js.json2xml_str(saefi))
+    }))
+    //xmls.push(x2js.json2xml_str(saefi))
     //const string = JSON.stringify(reports)
-    const string = xmls.join("")
+    const string = x2js.json2xml_str(output) //xmls.join("")
 
     const dirs = RNFetchBlob.fs.dirs
     const fs = RNFetchBlob.fs
